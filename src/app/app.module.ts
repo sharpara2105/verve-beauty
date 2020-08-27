50704c06a141413ee2071bb29ce5656998375ede
sharpara2105 <sharpara.2105@gmail.com>
2020-08-28 02:02:33 +0530

    router link added for new product form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,6 @@ import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { environment } from 'src/environments/environment';
 import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
-import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { LoginComponent } from './login/login.component';
 import * as firebase from 'firebase';
 import { AuthService } from './auth.service';
@@ -22,6 +21,7 @@ import { CheckOutComponent } from './check-out/check-out.component';
 import { UserService } from './user.service';
 import { AdminAuthGuardService } from './admin-auth-guard.service';
 import { ProductFormComponent } from './admin/product-form/product-form.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 firebase.initializeApp(environment.firebase);
 @NgModule({
   declarations: [
@@ -30,6 +30,7 @@ firebase.initializeApp(environment.firebase);
     ShoppingCartComponent,
     LoginComponent,
     ProductFormComponent,
+    AdminProductsComponent,
   ],
   imports: [
     BrowserModule,
